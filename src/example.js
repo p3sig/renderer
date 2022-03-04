76663cbd5d4244b5d3e1a80ed4accf59fa5ee08b
Giumo Clanjor (哆啦比猫/兰威举) <cjxgm2@gmail.com>
2022-03-04 13:51:34 +0800

    Introduce typescript

---
+++ src/example.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function (p3rr) {
+    const canvas = document.querySelector('canvas');
+    const engine = new p3rr.Engine(canvas);
+
+    window.engine = engine;
+    // engine.texture("Just Rendered Image");
+
+    const pipeline = engine.pipeline("Example");
+
+    engine.program("Render 3D")
+        .shader('vertex', `
+        `)
+        // .shader('fragment', `
+        // `)
+    ;
+
+    // pipeline.pass("Render 3D")
+    //     .output("Just Rendered Image")
+    //     .program("Render 3D")
+    // ;
+
+    // let frame = 0;
+    // let renderer = document.querySelector('#renderer');
+    // requestAnimationFrame(function render() {
+    //     const time = frame / 60;
+    //     renderer.render("Example", time);
+
+    //     frame++;
+    //     requestAnimationFrame(render);
+    // });
+}).call(this, p3rr);
+
